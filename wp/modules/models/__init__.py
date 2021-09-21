5a64e44e7fc93b4c20498cb6c541e003e6547dc3
Maksim Sandybekov <maksim.sandybekov@live.de>
2021-09-22 01:29:21 +0200

    add iris dataset experimental setup

--- wp/modules/models/__init__.py
+++ wp/modules/models/__init__.py
@@ -4,3 +4,4 @@ from .utils import setup_growth, set_tf_log_level, disable_tf_logs
 
 from .fchollet import FcholletCNN, fchollet_cnn
 from .y_gal import ygal_cnn
+from .dnn_dense_dropout import dnn_dense_dropout
